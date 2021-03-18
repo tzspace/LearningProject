816912d7f46bf1329b9e8eb9b06455494f0d2b95
ted <tedzeng7313@gmail.com>
2021-03-18 22:59:44 +0800

    add learn project

---
+++ src/com/tz/learning/march/serialization/SerializationDemo.java
@@ -0,0 +1,9 @@
+package com.tz.learning.march.serialization;
+
+public class SerializationDemo {
+
+    public static void main(String[] args){
+        Student student = new Student();
+        student.setName();
+    }
+}
---
+++ src/com/tz/learning/march/serialization/Student.java
@@ -0,0 +1,42 @@
+package com.tz.learning.march.serialization;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    private String name;
+    private String description;
+    private int age;
+    private int score;
+
+    public String GetName(){
+        return name;
+    }
+
+    public void SetName(String name){
+        this.name = name;
+    }
+
+    public String GetDescription(){
+        return description;
+    }
+
+    public void SetDescription(String description){
+        this.description = description;
+    }
+
+    public int GetAge(){
+        return age;
+    }
+
+    public void SetAge(int age){
+        this.age = age;
+    }
+
+    public int GetScore(){
+        return score;
+    }
+
+    public void SetScore(int score){
+        this.score = score;
+    }
+}
