940b6f6f766365e91347e25769e1a947713ee6d5
ted <tedzeng7313@gmail.com>
2021-03-19 23:16:39 +0800

    add gcd one more implementation

--- src/com/tz/learning/march/practice/gcd.java
+++ src/com/tz/learning/march/practice/gcd.java
@@ -21,4 +21,15 @@ public class gcd {
         }
         return result;
     }
+
+    /*int a = 1000;
+        int b = 495;
+        List<Integer> list = new ArrayList<Integer>();
+        for(int i = 1; i <= Math.min(a,b); i++){
+            if(a%i == 0 && b%i ==0){
+                list.add(i);
+                System.out.printf("公因子：%d%n",i);
+            }
+        }
+        System.out.printf("最大公因子是：%d%n", Collections.max(list));*/
 }
