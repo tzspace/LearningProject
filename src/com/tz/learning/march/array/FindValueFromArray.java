b31a1353d4baacfd332d3ccf636d3a4b7756bd99
ted <tedzeng7313@gmail.com>
2021-03-22 23:21:22 +0800

    find value from array

---
+++ src/com/tz/learning/march/array/FindValueFromArray.java
@@ -0,0 +1,29 @@
+package com.tz.learning.march.array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class FindValueFromArray {
+    /*public static void main(String[] args){
+        int[] number = {98,33,22,10,5,3,1};
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入要查找的数字:");
+        int input = scanner.nextInt();
+        int flag = -1;
+        for(int i = 0; i < number.length; i++){
+            if(number[i] == input){
+                flag = i;
+            }
+        }
+        System.out.printf("查找的结果是:%d", flag);
+    }*/
+
+    /*public static void main(String[] args){
+        int[] number = {98,33,22,10,5,3,1};
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入要查找的数字:");
+        int input = scanner.nextInt();
+
+        System.out.println(Arrays.asList(number).contains(input));
+    }*/
+}
