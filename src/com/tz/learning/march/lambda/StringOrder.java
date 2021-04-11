d28f2a9f08c528e3f4fae2ab29ee3760104f4f87
ted <tedzeng7313@gmail.com>
2021-04-11 22:45:06 +0800

    add lambda example

---
+++ src/com/tz/learning/march/lambda/StringOrder.java
@@ -0,0 +1,7 @@
+package com.tz.learning.march.lambda;
+
+public class StringOrder {
+    public static int ByLength(String name1, String name2){
+        return name1.length() - name2.length();
+    }
+}
