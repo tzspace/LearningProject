555da117fbaa2747a01e7cb009e15013719cdcbd
ted <tedzeng7313@gmail.com>
2021-04-13 23:22:53 +0800

    add log example

--- src/com/tz/learning/april/LoggerTesting.java
+++ src/com/tz/learning/april/LoggerTesting.java
@@ -5,9 +5,15 @@ import java.util.logging.*;
 public class LoggerTesting {
     public static void main(String[] args) {
         Logger logger = Logger.getLogger(LoggerTesting.class.getName());
+        logger.setLevel(Level.CONFIG);
+        for(Handler handler : logger.getParent().getHandlers()){
+            handler.setLevel(Level.CONFIG);
+        }
+        logger.log(Level.FINE, "信息");
+        logger.log(Level.CONFIG, "信息");
         logger.log(Level.WARNING, "警告");
         logger.log(Level.INFO, "信息");
-        logger.log(Level.CONFIG, "信息");
-        logger.log(Level.FINE, "信息");
+        //logger.log(Level.CONFIG, "信息");
+        //logger.log(Level.FINE, "信息");
     }
 }
